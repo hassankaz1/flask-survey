0750f7cbb11a0711273de7f90e3bc73fabc799b4
Hassan Kazi <andaijja@gmail.com>
2022-03-14 19:08:24 -0400

    finished session

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from http.client import responses
-from flask import Flask, redirect, render_template, request, flash
+from flask import Flask, redirect, render_template, request, flash, session
 from surveys import Question, surveys
 
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "secret"
 
-responses = []
+RESPONSES_KEY = "responses"
 
 
 @app.route("/")
@@ -16,22 +16,25 @@ def home_page():
     return render_template("home.html")
 
 
-@app.route("/questions/<number>")
+@app.route("/questions/<int:number>")
 def show_question(number):
+    # when survey starts we make session[response] = empty list
+    if number == 0:
+        session[RESPONSES_KEY] = []
+
+    responses = session[RESPONSES_KEY]
+
     # if all questions responded then return to home page
     if(len(responses) == len(surveys["satisfaction"].questions)):
         return redirect("/finished")
 
-    # turn number to int from string
-    num = int(number)
-
     # check if the user input number themselves if they do, flash message and redirect to current question
-    if(len(responses) != num):
-        flash(f"Invalid question id: {num}")
+    if(len(responses) != number):
+        flash(f"Invalid question id: {number}")
         return redirect(f"/questions/{len(responses)}")
 
     # get current question from survey
-    curr_q = surveys["satisfaction"].questions[num]
+    curr_q = surveys["satisfaction"].questions[number]
     return render_template("questions.html", q=curr_q)
 
 
@@ -39,7 +42,10 @@ def show_question(number):
 def next_question():
     # get the response and append to our response list
     ans = request.form['answer']
+    responses = session[RESPONSES_KEY]
     responses.append(ans)
+    session[RESPONSES_KEY] = responses
+
     # redirect back to questions
     return redirect(f"/questions/{len(responses)}")
 
